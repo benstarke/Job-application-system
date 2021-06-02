1854c48220bab0197c69c484bca4165726a5cfc3
Onjomba Felix <73028636+Developer-Felix@users.noreply.github.com>
2021-06-02 04:25:26 -0600

    resume upload attribute

--- jobsapp/models.py
+++ jobsapp/models.py
@@ -43,6 +43,7 @@ class Job(models.Model):
 
 class Applicant(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    upload_resume = models.FileField(upload_to='documents')
     job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name="applicants")
     created_at = models.DateTimeField(default=timezone.now)
     comment = models.TextField(blank=True, null=True)
